ff0be039fc21be8551fe408a9d163e945a715a15
kimtool <kimtool@hotmail.com>
2019-12-02 19:59:35 +0100

    register works like login

--- in28min-tutorial kopie2/DDDemoApp/src/components/DemoApp/RegisterComponent.jsx
+++ in28min-tutorial kopie2/DDDemoApp/src/components/DemoApp/RegisterComponent.jsx
@@ -1,19 +1,21 @@
 import React, {Component} from 'react'
+import AuthenticationService from './AuthenticationService.js'
 import Hexagon_logo_vector from '../images/Hexagon logo Don Diablo vector FillWhite.png'
 import {Link} from 'react-router-dom'
-import {SERVER_URL} from './constants.js';
 import '../../App.css'
 
 //control component the React component that renders a form 
 //also controls what happens in that form on subsequent user input (this.state)
 class LoginComponent extends Component {    
     constructor(props){
-        super(props);
-        this.state = { users: []};
+        super(props)
+        this.state = {
+            username: "",
+            password: "",
+            wasLoginSuccesful: false,
+            showErrorMessage: false
+        }
     }
-
-    // const [demo, setDemo] = useState({
-    //     name:'', email: '', password: ''});
 
 //handles change for all input fields
     handleChange = (event) => {
@@ -24,31 +26,17 @@ class LoginComponent extends Component {
             }
         )
     }
-
-    // let data = new FormData();
-    //     data.append('file', this.state.file);
-    //     data.append('name', this.state.file.name);
-
-    fetchDemos=()=>{
-        fetch(`${SERVER_URL}/members`)
-        .then((response) => response.json())
-        .then((responseData) => {
-            this.setState({
-                users: responseData._embedded.members,
-            });
-        }).catch(err => console.error(err));
-    }
-
-    addUser(user){
-        fetch(`${SERVER_URL}/members`, {method: 'POST', headers: {"Content-Type": "application/json",},
-    body:JSON.stringify(user)})
-    .then(res => this.fetchDemos())
-    .catch(err => console.error(err))
-    }     
-
-    // registerClicked = () => {
-    //     props.addUser(user);
-    // }
+    loginClicked = () => {
+//instead of password we need a token, token comes from response.data
+        AuthenticationService.executeJwtAuthenticationService(this.state.username,this.state.password)
+        .then((response) => {                
+            AuthenticationService.registerSuccessfulLoginForJws(this.state.username, response.data.token);
+            this.props.history.push(`/welcome/${this.state.username}`)
+            }).catch(() => {
+                this.setState({showErrorMessage:true})
+                this.setState({wasLoginSuccesful:false})
+            })
+        }  
 
     render(){
         return (
@@ -71,7 +59,7 @@ class LoginComponent extends Component {
                         <Link className="menu-icon" to="/login" target="blank" style={{color: "white"}}>Login</Link>
                     </div>              
                 </div> 
-                <button className="button" onClick={this.addUser}>Register</button>
+                <button className="button" onClick={this.loginClicked}>Register</button>
             </>
         );
     }
